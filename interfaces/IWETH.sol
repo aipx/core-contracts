45003d001048ec8d356ccffa510dc0cb30018c37
aiPX Protocol <156441453+aipx@users.noreply.github.com>
2024-02-16 12:16:09 -0600

    Create IWETH.sol

---
+++ interfaces/IWETH.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >= 0.8.0;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function transfer(address to, uint value) external returns (bool);
+    function withdraw(uint) external;
+}
